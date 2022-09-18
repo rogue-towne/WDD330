0060c820f2643ffc5524698e34649577a7415bae
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-09-18 11:52:07 -0600

    add week 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+      label: "Week 1",
+      url: "week1/index.html"
+    }
+  ]
+
+  const weekList = document.querySelector(".week-list")
+  links.forEach((item) => {
+    const a = document.createElement("a")
+    const li = document.createElement("li")
+    a.setAttribute('href', item.url) 
+    a.innerHTML = item.label
+    li.appendChild(a)
+    weekList.appendChild(li)
+})
